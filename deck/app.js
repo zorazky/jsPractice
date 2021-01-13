a33d9e61d643c83227e77c12d9085d1e4dfdc94f
zorazky <oswaldo.vazquez@gmail.com>
2021-01-13 13:45:19 -0600

    deck of cards sample

---
+++ deck/app.js
@@ -0,0 +1,39 @@
+
+
+const myDeck = {
+    deck: [],
+    drawnCards: [],
+    suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+    initializeDeck() {
+        const { suits, values, deck } = this;
+        for (let value of values.split(',')) {
+            for (let suit of values) {
+                deck.push({
+                    value,
+                    suit
+                })
+            }
+        }
+        // return deck;
+    },
+    drawCard() {
+        const card = this.deck.pop();
+        this.drawnCards.push(card);
+        return card;
+    },
+    drawMultiple(numCards) {
+        const cards = [];
+        for (let i = 0; i < numCards; i++) {
+            cards.push(this.drawCard());
+        }
+        return cards;
+    },
+    shuffle() {
+        const { deck } = this;
+        for (let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+    }
+}
